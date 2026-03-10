46c127a83171ca309a5ecd4137f5626ee57776e8
19h47 <jeremylevron@19h47.fr>
2026-03-10 14:06:32 +0100

    :sparkles: Fix build page

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
       '@agencecinq/utils': resolve(__dirname, '../../packages/utils/src/index.ts'),
       '@agencecinq/drawer': resolve(__dirname, '../../packages/drawer/src/index.ts'),
       '@agencecinq/modal': resolve(__dirname, '../../packages/modal/src/index.ts'),
+      '@agencecinq/tabs': resolve(__dirname, '../../packages/tabs/src/index.ts')
     }
   }
 });
